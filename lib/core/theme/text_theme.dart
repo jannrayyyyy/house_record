b35e8c6da15ee0f96c9ef310038ecde29a774aa6
Jannray Mostajo <jannmostajo02@gmail.com>
2022-07-29 08:04:01 -0700

    commit

---
+++ lib/core/theme/text_theme.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'colors.dart';
+
+var quicksandTextTheme = TextTheme(
+  headline1: GoogleFonts.quicksand(
+    fontSize: 97,
+    fontWeight: FontWeight.w300,
+    letterSpacing: -1.5,
+    color: color3,
+  ),
+  headline2: GoogleFonts.quicksand(
+    fontSize: 61,
+    fontWeight: FontWeight.w300,
+    letterSpacing: -0.5,
+    color: color3,
+  ),
+  headline3: GoogleFonts.quicksand(
+    fontSize: 48,
+    fontWeight: FontWeight.w400,
+    color: color3,
+  ),
+  headline4: GoogleFonts.quicksand(
+    fontSize: 34,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.25,
+    color: color3,
+  ),
+  headline5: GoogleFonts.quicksand(
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+    color: color3,
+  ),
+  headline6: GoogleFonts.quicksand(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0.15,
+    color: color3,
+  ),
+  subtitle1: GoogleFonts.quicksand(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.15,
+    color: color3,
+  ),
+  subtitle2: GoogleFonts.quicksand(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 0.1,
+    color: color3,
+  ),
+  bodyText1: GoogleFonts.quicksand(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.5,
+    color: color3,
+  ),
+  bodyText2: GoogleFonts.quicksand(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.25,
+    color: color3,
+  ),
+  button: GoogleFonts.quicksand(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    letterSpacing: 1.25,
+    color: color3,
+  ),
+  caption: GoogleFonts.quicksand(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 0.4,
+    color: color3,
+  ),
+  overline: GoogleFonts.quicksand(
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    letterSpacing: 1.5,
+    color: color3,
+  ),
+);
---
+++ lib/presentation/cubit/getrolecubit/getrole_state.dart
@@ -0,0 +1,29 @@
+part of 'getrole_cubit.dart';
+
+@immutable
+abstract class UserState {}
+
+class UserInitial extends UserState {}
+
+class SuperAdmin extends UserState {
+  final UserEntity user;
+  SuperAdmin({
+    required this.user,
+  });
+}
+
+class Admin extends UserState {
+  final UserEntity user;
+  Admin({
+    required this.user,
+  });
+}
+
+class UserLoading extends UserState {}
+
+class FirestoreUser extends UserState {
+  final UserEntity user;
+  FirestoreUser({
+    required this.user,
+  });
+}
